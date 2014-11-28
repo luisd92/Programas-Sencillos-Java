38bb93d87ccb30eb1bcb3e199977fd14a9f9d076
Luis Diego H <luisd1992@gmail.com>
2014-11-28 03:02:48 -0600

    Molino de String

---
+++ STRINGMARAVILLOSO.java
@@ -0,0 +1,43 @@
+//soy maravillos dos fors anidados se divide en dos partes superios e inferior 
+public class STRINGMARAVILLOSO{
+    public static void main(String[] args){
+        String M = "maravilloso";
+        
+        //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)
+        for (int i =0; i<M.length();i++){
+            //Imprime espacios de la primera parte para el primer triangulo 
+            for (int j=0; j < i  ;++j){
+                System.out.print(" ");
+            }
+            //Imprime el primer triangulo de la primer parte 
+            for (int j=i; j < M.length() ;++j){
+                System.out.print(M.charAt(j));
+            }
+            //Imprime espacios entre el primero y segundo triangulo de la primera parte
+            for (int j=i; j < M.length() ;++j){
+                System.out.print(" ");
+            }
+            //Imprime el segundo triangulo de la primera parte
+            for (int j= M.length() - i - 1; j < M.length(); ++j){
+                System.out.print(M.charAt(j));
+            }
+            System.out.println();
+        }
+        //Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
+        for (int i =0; i<M.length();i++){
+            //Imprime primer triangulo de la segunda parte
+            for (int j= i; j<M.length(); ++j){
+                System.out.print(M.charAt(j));
+            }
+            //Imprime espacios entre primer y segundo triangulo
+            for (int j=0; j <= i ;++j){
+                System.out.print(" ");
+            }
+            //Imprime segundo triangulo de la segunda parte  
+            for(int j=0; j < i + 1; ++j){
+                System.out.print(M.charAt(j));
+            }
+            System.out.println();
+        }
+    }    
+}
